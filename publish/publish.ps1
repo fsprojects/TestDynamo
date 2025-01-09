9c02f58448ab0f8f6bff9741ba18e4a8dd874179
Shane Connon <shane.connon@gmail.com>
2025-01-09 19:00:45 +0000

    Publish file refactor
     * Removed version file and replaced with "flags"
     * Flags are empty files in a flags directory that can track the progress of the build

--- publish/publish.ps1
+++ publish/publish.ps1
@@ -1,99 +1,146 @@
 param(
     [Parameter(Mandatory=$true)][string]$version,
-    $testOldVersions = $true)
+    $testOldVersions = $true,
+    $deleteBranchOnCompletion = $true)
 
-# back 2 directories from the current file
-$rootDir = ($MyInvocation.MyCommand.Path | Split-Path | Split-Path)
 
-$versionFile = "$rootDir\v$version.txt"
-$versionTag = "v$version"
-$versionBranch = "working-v$version"
-
-$testDynamoDll = "$rootDir\TestDynamo\TestDynamo.fsproj"
-
-$publishApps = @(
-    $testDynamoDll,
-    "$rootDir\TestDynamo.Lambda\TestDynamo.Lambda.fsproj",
-    "$rootDir\TestDynamo.Serialization\TestDynamo.Serialization.fsproj")
-
-$testApps = @(
-    "$rootDir\tests\TestDynamo.Tests\TestDynamo.Tests.fsproj",
-    "$rootDir\tests\TestDynamo.Tests.CSharp\TestDynamo.Tests.CSharp.csproj")
-
-function Mid-Execution {
-    Test-Path $versionFile
+if ($PSVersionTable.PSVersion.Major -lt 7 -or $PSVersionTable.PSVersion.Minor -lt 4) {
+    # $ErrorActionPreference, $PSNativeCommandUseErrorActionPreference feature
+    Write-Error "This script includes features which are only supported in powershell >= 7.4"
+    exit 1
 }
 
-Write-Host ""
-Write-Host "### Step 1: test project with older versions of dynamo db" -ForegroundColor Green
-$mx = Mid-Execution
-if (!$mx -and $testOldVersions) {
-    $dynamoVersions = @(
-        @("DYNAMO_V379", "3.7.404.11"),   # 2024-12-26
-        @("DYNAMO_V370", "3.7.0")         # 2021-03-06
-        # TODO
-        # @("DYNAMO_V359", "3.5.4.38"),     # 2021-03-25
-        # @("DYNAMO_V350", "3.5.0"),        # 2020-08-20
-        # @("DYNAMO_V339", "3.3.5")         # 2020-08-19
-        )
+$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = "Stop"
 
-    $dynamoVersions |
-        ForEach-Object -Process {
-            Write-Host "Testing with dynamodb version $($_[0])/$($_[1])"
-            Invoke-Expression "$rootDir\tools\testWithCustomDynamoLib.ps1 -libVersionId `"$($_[0])`" -libVersion `"$($_[1])`""
-            if (!$?) {
-                exit 1
-            }
-        }
-} elseif ($testOldVersions) {
-    Write-Host "Skipping old version test. Mid execution"
-}
-
-Write-Host ""
-Write-Host "### Step 2: make sure repo is clean. This process will create tags and branches and commit files" -ForegroundColor Green
 $diff = (git diff)
 if ($diff) {
     Write-Error "Repository must not have any changes before pack"
     exit 1
 }
 
-if (!$mx) {
+# back 2 directories from the current file
+$rootDir = ($MyInvocation.MyCommand.Path | Split-Path | Split-Path)
+
+$versionTag = "v$version"
+$versionBranch = "working-v$version"
+
+$testDynamoDll = "$rootDir\TestDynamo\TestDynamo.fsproj"
+$publishApps = @(
+    $testDynamoDll,
+    "$rootDir\TestDynamo.Lambda\TestDynamo.Lambda.fsproj",
+    "$rootDir\TestDynamo.Serialization\TestDynamo.Serialization.fsproj")
+$testApps = @(
+    "$rootDir\tests\TestDynamo.Tests\TestDynamo.Tests.fsproj",
+    "$rootDir\tests\TestDynamo.Tests.CSharp\TestDynamo.Tests.CSharp.csproj")
+
+$flagsDir = "$rootDir\publishFlags"
+
+function testFlag($flag) {
+    Test-Path "$flagsDir\$flag"
+}
+
+function setFlag($flag, $commitAll = $false) {
+    $exists = (Test-Path $flagsDir)
+    if (!$exists) {
+        New-Item -Path $flagsDir -Type Directory
+    }
+
+    Write-Host "" > "$flagsDir\$flag"
+    if ($commitAll) { git add . } 
+    else { git add "$flagsDir\$flag" }
+
+    git commit -m "Set flag: $flag"
+}
+
+$branchCreatedFlag = "branchCreated"
+$tmp = (testFlag $branchCreatedFlag)
+if (!$tmp) {
+    Write-Host ""
+    Write-Host "### Step 1: create release state (tag, branch and version file)" -ForegroundColor Green
+
+    if ((git branch --show-current) -ne "main") {
+        Write-Error "Deployments must be executed from main branch"
+        exit 1
+    }
+
+    git tag "$versionTag"
+    git checkout -b "$versionBranch"
+
+    setFlag $branchCreatedFlag
+} elseif ((git branch --show-current) -ne $versionBranch) {
+    Write-Error "Invalid branch config"
+    exit 1
+}
+
+$moveDtosFlag = "moveDtos"
+$tmp = (testFlag $moveDtosFlag)
+if (!$tmp) {
 
     Write-Host ""
-    Write-Host "### Step 3: merge GeneratedCode and TestDynamo projects" -ForegroundColor Green
+    Write-Host "### Step 2: merge GeneratedCode and TestDynamo projects" -ForegroundColor Green
     node "$rootDir\publish\project-parser\addGeneratedDtosIntoTestDynamo.js" `
         --testDynamo "$testDynamoDll" `
         --generatedCode "$rootDir\TestDynamo.GeneratedCode\TestDynamo.GeneratedCode.fsproj"
-    if (!$?) { exit 1 }
 
-    Write-Host ""
-    Write-Host "### Step 4: final test, with merged code, in release mode" -ForegroundColor Green
-    dotnet test "$rootDir" -c Release
-    if (!$?) { exit 1 }
-    
-    Write-Host ""
-    Write-Host "### Step 5: create release state (tag, branch and version file)" -ForegroundColor Green
-    git tag "$versionTag"
-    git checkout -b "$versionBranch"
-    New-Item -path "$rootDir" -name "v$version.txt" -type "file" -value ""
-    git add $versionFile
-    git commit -m "Added version file"
-} elseif ((git branch --show-current) -ne $versionBranch) {
-    Write-Error "Incorrect branch"
-    exit 1
-} else {
-    Write-Host ""
-    Write-Host "### Skipping steps 3 - 5" -ForegroundColor Green
+    setFlag $moveDtosFlag $true
 }
 
-Write-Host ""
-Write-Host "### Step 6: change dependant projects to reference the new release version of their dependencies" -ForegroundColor Green
-Write-Host "Pre processing project files"
-node "$rootDir\publish\project-parser\reformatFsProjForRelease.js" @($testApps + $publishApps) --version "$version"
-if (-not $?) { exit 1 }
+$executeTestsFlag = "executeTests"
+$tmp = (testFlag $executeTestsFlag)
+if (!$tmp) {
+    Write-Host ""
+    Write-Host "### Step 3: final test, with latest dynamodb + merged code, in release mode" -ForegroundColor Green
+    dotnet test "$rootDir" -c Release
+    
+    setFlag $executeTestsFlag $true
+}
 
-git add @($testApps + $publishApps)
-git commit -m "fsproj changes"
+$executeLegacyTestsFlag = "executeLegacyTests"
+$tmp = (testFlag $executeLegacyTestsFlag)
+if (!$tmp) {
+    
+    if ($testOldVersions) {
+        Write-Host ""
+        Write-Host "### Step 4: test project with older versions of dynamo db" -ForegroundColor Green
+
+        $dynamoVersions = @(
+            @("DYNAMO_V379", "3.7.404.11"),   # 2024-12-26
+            @("DYNAMO_V370", "3.7.0")         # 2021-03-06
+            # TODO
+            # @("DYNAMO_V359", "3.5.4.38"),     # 2021-03-25
+            # @("DYNAMO_V350", "3.5.0"),        # 2020-08-20
+            # @("DYNAMO_V339", "3.3.5")         # 2020-08-19
+            )
+    
+        $dynamoVersions |
+            ForEach-Object -Process {
+                Write-Host "Testing with dynamodb version $($_[0])/$($_[1])"
+                Invoke-Expression "$rootDir\tools\testWithCustomDynamoLib.ps1 -libVersionId `"$($_[0])`" -libVersion `"$($_[1])`""
+                if (!$?) {
+                    exit 1
+                }
+            }
+            
+        setFlag $executeLegacyTestsFlag
+    } else {
+        Write-Host ""
+        Write-Host "### Step 4: skipping old version test. Mid execution" -ForegroundColor Green
+    }
+}
+
+$updateDependenciesFlag = "updateDependencies"
+$tmp = (testFlag $updateDependenciesFlag)
+if (!$tmp) {
+    Write-Host ""
+    Write-Host "### Step 6: change dependant projects to reference the new release version of their dependencies" -ForegroundColor Green
+    Write-Host "Pre processing project files"
+
+    node "$rootDir\publish\project-parser\reformatFsProjForRelease.js" @($testApps + $publishApps) --version "$version"
+
+    git add @($testApps + $publishApps)    
+    setFlag $updateDependenciesFlag
+}
 
 function handle-pack-errors ($i, $err) {
     Write-Error "ERROR $err"
@@ -116,15 +163,18 @@ $nugetKey = ""
 $publishApps |
     ForEach-Object -Process {
 
+        $project = $_
+        $projectName = Split-Path $project -leaf
+        $projectFlag = "DEPLOYED_PROJECT_$projectName"
+        $tmp = (testFlag $projectFlag)
+        if ($tmp) {
+            return
+        }
+
         $i = 0
+        $ErrorActionPreference = "Continue"
         while ($true) {
             Write-Host "Packing $_, attempt $i"
-            $project = $_
-
-            if (Select-String -Path $versionFile -Pattern $project -SimpleMatch) {
-                Write-Host "Project $_ already done"
-                return
-            }
 
             Write-Host ""
             Write-Host "### Step 7: pack and release" -ForegroundColor Green
@@ -147,6 +197,8 @@ $publishApps |
 
             $i = $i + 1
         }
+        
+        $ErrorActionPreference = "Stop"
 
         $match = select-string "Successfully created package '([\w-\.\\/:]+).nupkg'" -inputobject $packResult
         if ($match) {
@@ -171,21 +223,15 @@ $publishApps |
                 "$_" `
                 -k "$nugetKey" `
                 --skip-duplicate
-                
-            if (-not $?) {
-                Write-Error "ERROR"
-                exit 1
-            }
         }
 
-        add-content -Path "$versionFile" -Value "$project"
-        git add $versionFile
-        git commit -m "Deployed $project"
+        setFlag $projectFlag
     }
 
 Write-Host ""
 Write-Host "### Step 8: run tests a final time, with reference to new deployed versions" -ForegroundColor Green
 $i = 0
+$ErrorActionPreference = "Continue"
 while ($True) {
     Write-Host "Infinite test loop. Will keep going until success"
     $success = $true
@@ -205,7 +251,11 @@ while ($True) {
 }
 
 git push origin "$versionTag"
-git reset HEAD --hard
-git checkout main
-git branch -D $versionBranch
+
+if ($deleteBranchOnCompletion) {
+    git reset HEAD --hard
+    git checkout main
+    git branch -D $versionBranch
+}
+
 Write-Host "Deploy complete"
